16836f1591ed51956d69b9616e2c4eb356c796a3
Logan Warwick <104666111+lwick95@users.noreply.github.com>
2022-06-26 22:15:36 -0500

    Update week-8-SQL-file.sql

--- week-8-SQL-file.sql
+++ week-8-SQL-file.sql
@@ -1,3 +1,14 @@
+SELECT count(birth_date) ,titles.title
+FROM employees
+INNER JOIN titles on employees.emp_no=titles.emp_no
+WHERE birth_date > '1965-01-01'
+GROUP BY title;
+
+SELECT avg(salary) AS "average salary", titles.title 
+FROM salaries
+INNER JOIN titles ON salaries.emp_no=titles.emp_no
+GROUP BY title;
+
 SELECT sum(salaries.salary) AS 'Sum of salary' ,
 departments.dept_name  AS 'Department'
 FROM salaries 
